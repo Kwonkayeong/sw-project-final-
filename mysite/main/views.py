33a1d1eb443f1bc63295441632ebd254ba4711fa
Kwonkayeong <kky3653@naver.com>
2022-06-21 07:18:22 +0000

    test commit

---
+++ mysite/main/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from .models import Post
+# Create your views here.
+
+def index(request):
+    postAll = Post.objects.all()
+    return render(request, 'main/index.html',
+{'postAll':postAll})
